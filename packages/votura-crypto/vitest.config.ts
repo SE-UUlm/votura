637a4a658cbbce5b80a229c744c5e9c9f9a99dbc
Thorben Carl <thorben.carl@gmail.com>
2025-05-08 21:24:29 +0200

    #57: setup vitest

--- packages/votura-crypto/vitest.config.ts
+++ packages/votura-crypto/vitest.config.ts
@@ -5,5 +5,15 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['test/**/*test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 80,
+        statements: 90,
+      },
+    },
   },
 });
