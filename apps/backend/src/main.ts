13192a95d579d4886a4dfbe87a0b4cd84de108c7
JKortmann <j.e.kortmann@gmail.com>
2025-05-08 11:10:00 +0200

    #60: add boilerplate for prisma

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,18 @@
+import {PrismaClient, type User} from "../generated/prisma/index.js"
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const allUsers: User[] = await prisma.user.findMany()
+    console.log(allUsers)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
