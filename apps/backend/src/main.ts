15657592d309ded4caa23ff64f72f907c680b045
JKortmann <j.e.kortmann@gmail.com>
2025-05-09 10:55:05 +0200

    #60: setup for eslint and prettier

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,18 +1,18 @@
-import {PrismaClient, type User} from "../generated/prisma/index.js"
+import { PrismaClient, type User } from '../generated/prisma/index.js';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    const allUsers: User[] = await prisma.user.findMany()
-    console.log(allUsers)
+  const allUsers: User[] = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ apps/docs/.prettierrc.js
@@ -0,0 +1,3 @@
+import { config } from '@repo/eslint-config/base-prettier';
+
+export default config;
