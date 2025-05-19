70815cb30d3cbbcedbc8b82e23372626aac3e58a
JKortmann <j.e.kortmann@gmail.com>
2025-05-19 08:25:44 +0200

    #79: adjust displayed version

--- apps/frontend/src/components/navbar/NavbarHeader.tsx
+++ apps/frontend/src/components/navbar/NavbarHeader.tsx
@@ -5,7 +5,7 @@ export const NavbarHeader = () => {
   return (
     <Group justify="space-between" h={HEADER_HEIGHT}>
       <Text fw={700}>Votura</Text>
-      <Code fw={700}>v0.0.1</Code>
+      <Code fw={700}>v0.0.0</Code>
     </Group>
   );
 };
