6ab601fea60c7ffa583202252aa1a78e5f081c34
JKortmann <j.e.kortmann@gmail.com>
2025-05-23 07:39:11 +0200

    #95: format

--- apps/backend/src/services/users.service.ts
+++ apps/backend/src/services/users.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from '../client.js';
-import {type User} from '../../generated/prisma/index.js';
+import { type User } from '../../generated/prisma/index.js';
 
 export const findUserById = (id: User['id']): Promise<User | null> => {
   return prisma.user.findUnique({ where: { id: id } });
