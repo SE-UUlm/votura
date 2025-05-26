9e1ca191975f2ecbe0c1c7b31354656f3579afa4
Thorben Carl <thorben.carl@gmail.com>
2025-05-26 12:43:52 +0200

    #80: add review changes

--- apps/backend/prisma/migrations/20250526104126_init/migration.sql
+++ apps/backend/prisma/migrations/20250526104126_init/migration.sql
@@ -1,55 +1,51 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "created" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "modified" TIMESTAMPTZ(6) NOT NULL,
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMPTZ(6) NOT NULL,
     "email" TEXT NOT NULL,
     "pwHash" TEXT NOT NULL,
     "verified" BOOLEAN NOT NULL DEFAULT false,
-    "lastLoginAt" TIMESTAMPTZ(6),
-    "failedLoginAttempts" INTEGER DEFAULT 0,
     "emailVerificationTokenHash" TEXT,
     "emailVerificationTokenExpiresAt" TIMESTAMPTZ(6),
     "passwordResetTokenHash" TEXT,
     "passwordResetTokenExpiresAt" TIMESTAMPTZ(6),
-    "passwordResetTokenUsedAt" TIMESTAMPTZ(6),
     "refreshTokenHash" TEXT,
     "refreshTokenExpiresAt" TIMESTAMPTZ(6),
-    "refreshTokenRevokedAt" TIMESTAMPTZ(6),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "modified_after_created" CHECK ("modified" >= "created") -- manually added
 );
 
 -- CreateTable
-CREATE TABLE "JwtBlacklist" (
-    "jti" TEXT NOT NULL,
-    "created" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "modified" TIMESTAMPTZ(6) NOT NULL,
+CREATE TABLE "AccessTokenBlacklist" (
+    "id" UUID NOT NULL,
+    "accessTokenId" UUID NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMPTZ(6) NOT NULL,
     "expiresAt" TIMESTAMPTZ(6) NOT NULL,
-    "blacklistedAt" TIMESTAMPTZ(6) NOT NULL,
 
-    CONSTRAINT "JwtBlacklist_pkey" PRIMARY KEY ("jti"),
+    CONSTRAINT "AccessTokenBlacklist_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "modified_after_created" CHECK ("modified" >= "created") -- manually added
 );
 
 -- CreateTable
 CREATE TABLE "Election" (
-    "id" TEXT NOT NULL,
-    "created" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "modified" TIMESTAMPTZ(6) NOT NULL,
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMPTZ(6) NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
-    "votingStart" TIMESTAMPTZ(6) NOT NULL,
-    "votingEnd" TIMESTAMPTZ(6) NOT NULL,
+    "votingStartAt" TIMESTAMPTZ(6) NOT NULL,
+    "votingEndAt" TIMESTAMPTZ(6) NOT NULL,
     "configFrozen" BOOLEAN NOT NULL DEFAULT false,
     "allowInvalidVotes" BOOLEAN NOT NULL DEFAULT false,
-    "pubKeyVotes" BIGINT,
-    "privKeyVotes" BIGINT,
+    "pubKey" BIGINT,
+    "privKey" BIGINT,
     "primeP" BIGINT,
     "primeQ" BIGINT,
     "generator" BIGINT,
-    "electionCreatorId" TEXT NOT NULL,
+    "electionCreatorId" UUID NOT NULL,
 
     CONSTRAINT "Election_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "votingEnd_after_votingStart" CHECK ("votingEnd" > "votingStart"), -- manually added
@@ -58,6 +54,9 @@ CREATE TABLE "Election" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AccessTokenBlacklist_accessTokenId_key" ON "AccessTokenBlacklist"("accessTokenId");
 
 -- AddForeignKey
 ALTER TABLE "Election" ADD CONSTRAINT "Election_electionCreatorId_fkey" FOREIGN KEY ("electionCreatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
