622a677c764723e90989bffb3c13f8994ff39ca4
JKortmann <j.e.kortmann@gmail.com>
2025-05-26 09:07:08 +0200

    #95: use nullish coalescing operator

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function main() {
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT ?? 3000;
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json()); // parse JSON bodies
