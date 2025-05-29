9fe0aa1e40b70758a6b2b1beb1ac55143f55053a
Sören S. Hoffmann <125372709+shoffma@users.noreply.github.com>
2025-05-29 14:56:21 +0200

    bring back old test

--- packages/votura-crypto/test/index.test.ts
+++ packages/votura-crypto/test/index.test.ts
@@ -207,6 +207,61 @@ describe('ZeroKnowledgeProof', () => {
     expect(isValid4).toBe(false);
   });
 
+  /* voturaTest('verifyDisjunctiveEncryptionProof', ({ keyPair, plaintext, randomness }) => {
+    const { publicKey } = keyPair;
+    const plaintexts = [plaintext, 123123123n, 456456456n, 789789789n];
+    const ciphertexts: Ciphertext[] = plaintexts.map((p) => publicKey.encrypt(p, randomness)[0]);
+    const realIndex = plaintexts.indexOf(plaintext);
+
+    const zkp = new ZeroKnowledgeProof(publicKey);
+    const validProofs = zkp.createDisjunctiveEncryptionProof(
+      ciphertexts,
+      realIndex,
+      randomness,
+    );
+
+    const isValid = zkp.verifyDisjunctiveEncryptionProof(ciphertexts, validProofs);
+    expect(isValid).toBe(true); // test fails here
+
+    const invalidProofs1: ZKProof[] = validProofs.map((proof, index) => ({
+      commitment:
+        index === 0
+          ? [(proof.commitment[0] + 1n) % publicKey.primeP, proof.commitment[1]]
+          : [proof.commitment[0], proof.commitment[1]],
+      challenge: proof.challenge,
+      response: proof.response,
+    }));
+    const isValid1 = zkp.verifyDisjunctiveEncryptionProof(ciphertexts, invalidProofs1);
+    expect(isValid1).toBe(false);
+
+    const invalidProofs2: ZKProof[] = validProofs.map((proof, index) => ({
+      commitment:
+        index === 1
+          ? [proof.commitment[0], (proof.commitment[1] + 1n) % publicKey.primeP]
+          : [proof.commitment[0], proof.commitment[1]],
+      challenge: proof.challenge,
+      response: proof.response,
+    }));
+    const isValid2 = zkp.verifyDisjunctiveEncryptionProof(ciphertexts, invalidProofs2);
+    expect(isValid2).toBe(false);
+
+    const invalidProofs3: ZKProof[] = validProofs.map((proof, index) => ({
+      commitment: [proof.commitment[0], proof.commitment[1]],
+      challenge: index === 2 ? (proof.challenge + 1n) % publicKey.primeQ : proof.challenge,
+      response: proof.response,
+    }));
+    const isValid3 = zkp.verifyDisjunctiveEncryptionProof(ciphertexts, invalidProofs3);
+    expect(isValid3).toBe(false);
+
+    const invalidProofs4: ZKProof[] = validProofs.map((proof, index) => ({
+      commitment: [proof.commitment[0], proof.commitment[1]],
+      challenge: proof.challenge,
+      response: index === 3 ? (proof.response + 1n) % publicKey.primeQ : proof.response,
+    }));
+    const isValid4 = zkp.verifyDisjunctiveEncryptionProof(ciphertexts, invalidProofs4);
+    expect(isValid4).toBe(false);
+  }); //*/
+
   voturaTest('verifyDecryptionProof', ({ keyPair, plaintext, ciphertext }) => {
     const { publicKey } = keyPair;
     const validProof = keyPair.privateKey.createDecryptionProof(ciphertext);
