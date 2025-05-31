39082e870328dc5932e225787a9e2d3505d1b508
Thorben Carl <thorben.carl@gmail.com>
2025-05-31 16:50:31 +0200

    #7: update vitest reporter

--- packages/votura-crypto/vitest.config.ts
+++ packages/votura-crypto/vitest.config.ts
@@ -5,9 +5,28 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['test/**/*test.ts'],
+    reporters: [
+      'verbose',
+      ['default', { summary: false }],
+      [
+        'junit',
+        {
+          suiteName: 'votura-crypto tests',
+          classnameTemplate: 'filename:{filename} - filepath:{filepath}',
+        },
+      ],
+      'json',
+      'html'
+    ],
+    outputFile: {
+      junit: './results/junit-report.xml',
+      json: './results/json-report.json',
+      html: './results/index.html',
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      include: ['src/**'],
       thresholds: {
         lines: 85,
         functions: 90,
