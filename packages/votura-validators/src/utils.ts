7c5ac4ab11f58d27d145b4138eb4b6faeba03122
JKortmann <j.e.kortmann@gmail.com>
2025-06-02 14:07:35 +0200

    #130: format

--- packages/votura-validators/src/utils.ts
+++ packages/votura-validators/src/utils.ts
@@ -5,18 +5,18 @@ export const voturaMetadataRegistry = z.registry<GlobalMeta>();
 export const IdentifiableObject = z.object({
   id: z.uuidv4().register(voturaMetadataRegistry, {
     description: 'The unique identifier of the object',
-    examples: ['123e4567-e89b-12d3-a456-426614174000']
+    examples: ['123e4567-e89b-12d3-a456-426614174000'],
   }),
 });
 
 export const TimestampedObject = z.object({
   createdAt: z.iso.datetime().register(voturaMetadataRegistry, {
     description: 'The creation date of the object.',
-    examples: ['2032-10-01T00:00:00Z']
+    examples: ['2032-10-01T00:00:00Z'],
   }),
   modifiedAt: z.iso.datetime().register(voturaMetadataRegistry, {
     description: 'The latest modification date of the object.',
-    examples: ['2032-10-01T00:00:00Z']
+    examples: ['2032-10-01T00:00:00Z'],
   }),
 });
 
