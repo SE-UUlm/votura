af5d5d7e84275fd974dcf2f0031b52da844d830b
JKortmann <j.e.kortmann@gmail.com>
2025-06-04 15:22:18 +0200

    #130: add tags

--- packages/votura-validators/src/schema/globals/tag.ts
+++ packages/votura-validators/src/schema/globals/tag.ts
@@ -1,4 +1,8 @@
 export enum Tag {
     Elections = 'Elections',
     Users = 'Users',
+    BallotPapers = 'BallotPapers',
+    BallotPaperSections = 'BallotPaperSections',
+    Candidates = 'Candidates',
+    VoterGroups = 'VoterGroups',
 }
