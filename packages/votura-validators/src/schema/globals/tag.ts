6e41587683c4da122c879998cf47d6e37c904b95
JKortmann <j.e.kortmann@gmail.com>
2025-06-05 14:03:49 +0200

    #130: format

--- packages/votura-validators/src/schema/globals/tag.ts
+++ packages/votura-validators/src/schema/globals/tag.ts
@@ -1,8 +1,8 @@
 export enum Tag {
-    Elections = 'Elections',
-    Users = 'Users',
-    BallotPapers = 'BallotPapers',
-    BallotPaperSections = 'BallotPaperSections',
-    Candidates = 'Candidates',
-    VoterGroups = 'VoterGroups',
+  Elections = 'Elections',
+  Users = 'Users',
+  BallotPapers = 'BallotPapers',
+  BallotPaperSections = 'BallotPaperSections',
+  Candidates = 'Candidates',
+  VoterGroups = 'VoterGroups',
 }
