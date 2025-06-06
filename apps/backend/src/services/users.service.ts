0ea045f47c738f9759e9acb836f02d097bf91143
David Sievers <david.sievers@uni-ulm.de>
2025-06-06 16:06:28 +0200

    #157 : setup kysley and remove prisma
    The way db.types.ts is currently (unmodified), creating a user requires a uuid from the backend and modifiedAt also has to be given from the backend.
    We could change that by editing the db.types.ts file but this would mean we are changing generated code.
    
    Signed-off-by: David Sievers <david.sievers@uni-ulm.de>

--- apps/backend/src/services/users.service.ts
+++ apps/backend/src/services/users.service.ts
@@ -1,6 +1,16 @@
-import { prisma } from '../client.js';
-import { type User } from '../../generated/prisma/index.js';
+//import { prisma } from '../client.js';
+//import { type User } from '../../generated/prisma/index.js';
+import {db} from '../db/database.js';
+import {type User} from '../db/db.types.js';
 
-export const findUserById = (id: User['id']): Promise<User | null> => {
-  return prisma.user.findUnique({ where: { id: id } });
-};
+//export const findUserById = (id: User['id']): Promise<User | null> => {
+//  return prisma.user.findUnique({ where: { id: id } });
+//};
+
+export async function findUserById(id: string): Promise<User | null> {
+  const user: any | undefined = await db.selectFrom('User')
+    .where('id', '=', id)
+    .selectAll()
+    .executeTakeFirst();
+  return user === undefined ? null : (user as User);
+}
