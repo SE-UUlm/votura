ae9af219d887338a8a50ee761dbdab1e2f4c93ee
David Sievers <david.sievers@uni-ulm.de>
2025-06-10 14:03:46 +0200

    #157: fix returntype of findUserById
    Signed-off-by: David Sievers <david.sievers@uni-ulm.de>

--- apps/backend/src/services/users.service.ts
+++ apps/backend/src/services/users.service.ts
@@ -1,11 +1,12 @@
 import { db } from '../db/database.js';
 import { type User } from '../db/db.types.js';
+import { type Selectable } from 'kysely';
 
-export async function findUserById(id: string): Promise<User | null> {
-  const user: any | undefined = await db
+export async function findUserById(id: string): Promise<Selectable<User> | null> {
+  const user: Selectable<User> | undefined = await db
     .selectFrom('User')
     .where('id', '=', id)
     .selectAll()
     .executeTakeFirst();
-  return user === undefined ? null : (user as User);
+  return user === undefined ? null : user;
 }
