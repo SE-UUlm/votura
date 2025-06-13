f6c11fa0d1ff72caa4782b1909b713b1521ba3c3
JKortmann <j.e.kortmann@gmail.com>
2025-06-13 08:36:37 +0200

    #130: add utils

---
+++ apps/backend/src/utils.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const spreadableOptional = <O extends Record<any, any>, K extends keyof O>(
+  object: O,
+  key: K,
+) => {
+  if (object[key] === null || object[key] === undefined) {
+    return {};
+  }
+
+  return {
+    [key]: object[key],
+  };
+};
