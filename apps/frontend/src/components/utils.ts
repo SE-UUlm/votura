af149c52307ff6253ad4fb264d4cdd8581bc1aa3
JKortmann <j.e.kortmann@gmail.com>
2025-06-13 10:08:25 +0200

    #163: fix magic numbers in frontend

--- apps/frontend/src/components/utils.ts
+++ apps/frontend/src/components/utils.ts
@@ -1,1 +1,4 @@
-export const HEADER_HEIGHT = 16 * 4; // Mantine declares 16 to be 1rem
+const BASE_FACTOR = 16; // Mantine declares 16 to be 1rem
+const BASE_FACTOR_MULTIPLIER = 4;
+
+export const HEADER_HEIGHT = BASE_FACTOR * BASE_FACTOR_MULTIPLIER;
