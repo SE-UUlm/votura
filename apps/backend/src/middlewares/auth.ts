c915f80f316a26018cd7abe96ce7bb084aa4572d
JKortmann <j.e.kortmann@gmail.com>
2025-06-13 08:37:27 +0200

    #130: refactor auth middlware

--- apps/backend/src/middlewares/auth.ts
+++ apps/backend/src/middlewares/auth.ts
@@ -1,12 +1,10 @@
-import { type Request, type Response, type NextFunction } from 'express';
-import { prisma } from '../client.js';
-import { type User } from './../../generated/prisma/index.js';
+import { type NextFunction, type Request, type Response } from 'express';
 import type { Response401 } from '@repo/votura-validators';
+import { findUserBy } from '../services/users.service.js';
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   // IMPLEMENT AUTHENTICATION HERE
-
-  const user: User | null = await prisma.user.findUnique({ where: { email: 'user@votura.org' } });
+  const user = await findUserBy({ email: 'user@votura.org' });
 
   if (!user) {
     const response: Response401 = {
