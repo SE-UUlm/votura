8c7a987db5b6b857a29696ab8f9efa2e94c8fcb2
JKortmann <j.e.kortmann@gmail.com>
2025-06-13 08:37:16 +0200

    #130: refactor election controller

--- apps/backend/src/controllers/elections.controllers.ts
+++ apps/backend/src/controllers/elections.controllers.ts
@@ -1,22 +1,31 @@
 import { type Request, type Response } from 'express';
 import {
-  InsertableElectionObject,
+  insertableElectionObject,
+  type Response500,
   type SelectableElection,
+  type SelectableUser,
   zodErrorToResponse400,
 } from '@repo/votura-validators';
 import { type Response400 } from '@repo/votura-validators';
 import { createElection as createPersistentElection } from '../services/elections.service.js';
-import { type User } from './../../generated/prisma/index.js';
 
-export type CreateElectionResponse = Response<SelectableElection | Response400, { user: User }>;
+export type CreateElectionResponse = Response<
+  SelectableElection | Response400 | Response500,
+  { user: SelectableUser }
+>;
 
 export const createElection = async (req: Request, res: CreateElectionResponse) => {
   const body: unknown = req.body;
 
-  const { data, error, success } = await InsertableElectionObject.safeParseAsync(body);
+  const { data, error, success } = await insertableElectionObject.safeParseAsync(body);
 
   if (success) {
     const selectableElection = await createPersistentElection(data, res.locals.user.id);
+
+    if (!selectableElection) {
+      res.sendStatus(500);
+      return;
+    }
 
     res.status(201).send(selectableElection);
   } else {
