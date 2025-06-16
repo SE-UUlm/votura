98c00f577f95991fef907555a198abb0b5857f83
Thorben Carl <thorben.carl@gmail.com>
2025-06-16 19:54:00 +0200

    #96: debugging

--- apps/backend/src/db/database.ts
+++ apps/backend/src/db/database.ts
@@ -4,10 +4,13 @@ import { Kysely, PostgresDialect } from 'kysely';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const databaseUrl =
+  process.env.DATABASE_URL ?? 'postgresql://votura:votura@localhost:5432/votura?schema=public';
+
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
     pool: new Pool({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: databaseUrl,
     }),
   }),
 });
