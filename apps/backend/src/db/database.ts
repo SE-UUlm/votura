920ced7339c036c3ddefb7ed8b21fd4f99c86732
Thorben Carl <thorben.carl@gmail.com>
2025-06-23 13:00:38 +0200

    #152: add kysely logger

--- apps/backend/src/db/database.ts
+++ apps/backend/src/db/database.ts
@@ -1,10 +1,29 @@
 import dotenv from 'dotenv';
-import { Kysely, PostgresDialect } from 'kysely';
+import { Kysely, PostgresDialect, type LogEvent } from 'kysely';
 import { Pool } from 'pg';
+import logger from './../logger.js';
 import type { DB } from './types/db.js';
+
 dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL;
+
+export const kyselyLogger = (event: LogEvent): void => {
+  if (event.level === 'error') {
+    logger.error({ event }, 'DB query error');
+  }
+
+  if (event.level === 'query') {
+    logger.debug(
+      {
+        query: event.query.sql,
+        params: event.query.parameters,
+        duration: event.queryDurationMillis,
+      },
+      'DB query executed',
+    );
+  }
+};
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
@@ -12,4 +31,5 @@ export const db = new Kysely<DB>({
       connectionString: databaseUrl,
     }),
   }),
+  log: kyselyLogger,
 });
