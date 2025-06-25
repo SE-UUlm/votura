93ec09fd73494292d1add800809196450cce0a93
Thorben Carl <thorben.carl@gmail.com>
2025-06-25 17:36:11 +0200

    #132: enhance deleteElection test to verify cascade deletion of ballot paper

--- apps/backend/test/elections/deleteElection.test.ts
+++ apps/backend/test/elections/deleteElection.test.ts
@@ -1,16 +1,23 @@
-import { parameter, type SelectableElection, type SelectableUser } from '@repo/votura-validators';
+import {
+  parameter,
+  type SelectableBallotPaper,
+  type SelectableElection,
+  type SelectableUser,
+} from '@repo/votura-validators';
 import request from 'supertest';
 import { beforeAll, describe, expect, it } from 'vitest';
 import { app } from '../../src/app.js';
 import { HttpStatusCode } from '../../src/httpStatusCode.js';
 import { createUser, findUserBy } from '../../src/services/users.service.js';
-import { DEMO_TOKEN, demoElection, demoUser } from '../mockData.js';
+import { DEMO_TOKEN, demoBallotPaper, demoElection, demoUser } from '../mockData.js';
+import { createBallotPaper, getBallotPaper } from './../../src/services/ballotPapers.service.js';
 import { createElection, getElection } from './../../src/services/elections.service.js';
 
 describe(`DEL /elections/:${parameter.electionId}`, () => {
   let requestPath = '';
   let user: SelectableUser | null = null;
   let election: SelectableElection | null = null;
+  let ballotPaper: SelectableBallotPaper | null = null;
 
   beforeAll(async () => {
     await createUser(demoUser);
@@ -24,12 +31,16 @@ describe(`DEL /elections/:${parameter.electionId}`, () => {
       throw new Error('Failed to create test election');
     }
 
+    ballotPaper = await createBallotPaper(demoBallotPaper, election.id);
+    if (ballotPaper === null) {
+      throw new Error('Failed to create test ballot paper');
+    }
+
     requestPath = `/elections/${election.id}`;
   });
 
   it('203: should delete an election', async () => {
     const res = await request(app).delete(requestPath).set('Authorization', DEMO_TOKEN);
-
     expect(res.status).toBe(HttpStatusCode.NoContent);
     if (election?.id === undefined) {
       throw new Error('Election ID is undefined');
@@ -39,5 +50,12 @@ describe(`DEL /elections/:${parameter.electionId}`, () => {
     }
     const dbResult = await getElection(election?.id, user?.id);
     expect(dbResult).toBeNull();
+
+    // Check cascade deletion of ballot paper
+    if (ballotPaper?.id === undefined) {
+      throw new Error('Ballot Paper ID is undefined');
+    }
+    const ballotPaperResult = await getBallotPaper(ballotPaper?.id);
+    expect(ballotPaperResult).toBeNull();
   });
 });
