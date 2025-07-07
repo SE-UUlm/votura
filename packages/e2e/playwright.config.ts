0d47dedfed9af0c87397f61836ff8b7aeeeead16
JKortmann <j.e.kortmann@gmail.com>
2025-07-07 14:51:56 +0200

    #173: refactor application setup

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -2,8 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import * as os from 'node:os';
 import { fileURLToPath } from 'node:url';
+import path from 'path';
 
 dotenv.config({ path: fileURLToPath(new URL('.env', import.meta.url)) });
+const FILENAME = fileURLToPath(import.meta.url);
+const DIRNAME = path.dirname(FILENAME);
 
 export default defineConfig({
   testDir: './tests',
@@ -15,6 +18,27 @@ export default defineConfig({
   retries: process.env.CI != null ? 2 : 0,
   workers: process.env.CI != null ? 1 : os.availableParallelism(),
   reporter: 'html',
+  webServer: [
+    {
+      command: 'npm run start',
+      url: 'http://localhost:4000',
+      cwd: path.join(DIRNAME, '../../apps/backend'),
+      env: {
+        ...process.env,
+        PORT: '4000',
+        PEPPER: '1234',
+      },
+    },
+    {
+      command: 'npm run start',
+      url: 'http://localhost:5173',
+      cwd: path.join(DIRNAME, '../../apps/frontend'),
+      env: {
+        ...process.env,
+        VITE_API_BASE_URL: 'http://localhost:4000',
+      },
+    },
+  ],
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
--- packages/e2e/testEnv.ts
+++ packages/e2e/testEnv.ts
@@ -2,16 +2,12 @@ import { migrateToLatest } from '@repo/db/migrateToLatest';
 import type { DB } from '@repo/db/types';
 import { kyselyLogger, logger } from '@repo/logger';
 import { PostgreSqlContainer, type StartedPostgreSqlContainer } from '@testcontainers/postgresql';
-import { type ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import { Kysely, PostgresDialect } from 'kysely';
 import path from 'path';
 import { Pool } from 'pg';
 import { fileURLToPath } from 'url';
-import waitOn from 'wait-on';
 
 let dbContainer: StartedPostgreSqlContainer;
-let backendProcess: ChildProcessWithoutNullStreams;
-let frontendProcess: ChildProcessWithoutNullStreams;
 
 const FILENAME = fileURLToPath(import.meta.url);
 const DIRNAME = path.dirname(FILENAME);
@@ -19,13 +15,15 @@ const DIRNAME = path.dirname(FILENAME);
 export const startTestEnv = async (): Promise<void> => {
   logger.info('Creating postgres container...');
   dbContainer = await new PostgreSqlContainer('postgres:17.4').start();
+  const dbConnectionUri = dbContainer.getConnectionUri();
+  process.env.DATABASE_URL = dbConnectionUri;
   logger.info('Postgres container created.');
 
   logger.info('Running migration...');
   const migrationClient = new Kysely<DB>({
     dialect: new PostgresDialect({
       pool: new Pool({
-        connectionString: dbContainer.getConnectionUri(),
+        connectionString: dbConnectionUri,
       }),
     }),
     log: kyselyLogger,
@@ -34,45 +32,8 @@ export const startTestEnv = async (): Promise<void> => {
   const migrationPath = path.join(DIRNAME, '../db/src/migrations');
   await migrateToLatest(migrationClient, migrationPath);
   logger.info('Migration completed.');
-
-  logger.info('Spawning backend process...');
-  backendProcess = spawn('npm', ['run', 'start'], {
-    cwd: path.join(DIRNAME, '../../apps/backend'),
-    env: {
-      ...process.env,
-      DATABASE_URL: dbContainer.getConnectionUri(),
-      PORT: '4000',
-      PEPPER: '1234',
-    },
-  });
-
-  logger.info('Waiting for backend process...');
-  await waitOn({
-    resources: ['http://localhost:4000'],
-    timeout: 20000,
-    validateStatus: (status) => {
-      logger.info(status);
-      return status === 400;
-    },
-  });
-  logger.info('Backend process spawned.');
-
-  logger.info('Spawning frontend process...');
-  frontendProcess = spawn('npm', ['run', 'start'], {
-    cwd: '../../apps/frontend',
-    env: {
-      ...process.env,
-      VITE_API_BASE_URL: 'http://localhost:4000',
-    },
-  });
-
-  logger.info('Waiting for frontend process...');
-  await waitOn({ resources: ['http://localhost:5173'] });
-  logger.info('Frontend process spawned.');
 };
 
 export const stopTestEnv = async (): Promise<void> => {
   await dbContainer?.stop();
-  backendProcess?.kill();
-  frontendProcess?.kill();
 };
