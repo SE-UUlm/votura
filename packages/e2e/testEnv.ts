4bd20dd914ef9234d64e532ab62f3cef52148c04
JKortmann <j.e.kortmann@gmail.com>
2025-07-07 15:34:21 +0200

    #173: format

--- packages/e2e/testEnv.ts
+++ packages/e2e/testEnv.ts
@@ -17,7 +17,7 @@ export const startTestEnv = async (): Promise<void> => {
   dbContainer = await new PostgreSqlContainer('postgres:17.4').start();
   const dbConnectionUri = dbContainer.getConnectionUri();
   process.env.DATABASE_URL = dbConnectionUri;
-  process.env.HOME = '/root'
+  process.env.HOME = '/root';
   logger.info('Postgres container created.');
 
   logger.info('Running migration...');
