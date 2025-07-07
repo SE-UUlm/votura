c91dc6382e4029110e4535f7a176ffe02258ec00
JKortmann <j.e.kortmann@gmail.com>
2025-07-07 16:14:41 +0200

    #200: add central api error handling

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,20 +1,36 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
-import { Notifications } from '@mantine/notifications';
+import { notifications, Notifications } from '@mantine/notifications';
 import '@mantine/notifications/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { SWRConfig } from 'swr';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider>
-      <Notifications />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MantineProvider>
+    <SWRConfig
+      value={{
+        onError: (err) => {
+          console.error(err);
+          notifications.show({
+            title: 'Error',
+            message:
+              'We encountered a networking issue. Please try again or get in contact with us',
+            autoClose: 10000,
+            color: 'red',
+          });
+        },
+      }}
+    >
+      <MantineProvider>
+        <Notifications />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MantineProvider>
+    </SWRConfig>
   </StrictMode>,
 );
