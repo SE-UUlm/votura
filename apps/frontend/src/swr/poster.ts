8cbb4ee0d400f761ee211c9ea2acd4fb694fb13d
JKortmann <j.e.kortmann@gmail.com>
2025-07-12 17:24:28 +0200

    #127: refactor to axios client

--- apps/frontend/src/swr/poster.ts
+++ apps/frontend/src/swr/poster.ts
@@ -1,26 +1,27 @@
-import { apiRoutes } from './apiRoutes.ts';
+import axios from 'axios';
+import { api } from './api.ts';
 import { hasMessage } from './hasMessage.ts';
 
 export const poster = async <T>(url: string, args: { arg: T }): Promise<unknown> => {
-  const response = await fetch(apiRoutes.base + url, {
-    method: 'POST',
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Accept: 'application/json',
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(args.arg),
-  });
+  try {
+    const response = await api.post(url, args.arg, {
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Accept: 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+      },
+    });
 
-  const data: unknown = await response.json();
-
-  if (!response.ok) {
-    const errorMessage = hasMessage(data)
-      ? data.message
-      : 'We encountered an unexpected error while creating a resource. Please try again later or get in contact with us.';
-    throw new Error(errorMessage);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const errorMessage = hasMessage(error.response)
+        ? error.response.message
+        : 'We encountered an unexpected error while creating a resource. Please try again later or get in contact with us.';
+      throw new Error(errorMessage);
+    } else {
+      throw error;
+    }
   }
-
-  return data;
 };
