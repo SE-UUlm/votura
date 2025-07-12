c5ab0368b6aa02e367be54e1c15ca841dd3a39d9
JKortmann <j.e.kortmann@gmail.com>
2025-07-12 17:24:25 +0200

    #127: refactor to axios client

--- apps/frontend/src/swr/getter.ts
+++ apps/frontend/src/swr/getter.ts
@@ -1,23 +1,25 @@
-import { apiRoutes } from './apiRoutes.ts';
+import axios from 'axios';
+import { api } from './api.ts';
 import { hasMessage } from './hasMessage.ts';
 
 export const getter = async (url: string): Promise<unknown> => {
-  const response = await fetch(apiRoutes.base + url, {
-    method: 'GET',
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Accept: 'application/json',
-    },
-  });
+  try {
+    const response = await api.get(url, {
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Accept: 'application/json',
+      },
+    });
 
-  const data: unknown = await response.json();
-
-  if (!response.ok) {
-    const errorMessage = hasMessage(data)
-      ? data.message
-      : 'We encountered an unexpected error while fetching a resource. Please try again later or get in contact with us.';
-    throw new Error(errorMessage);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const errorMessage = hasMessage(error.response)
+        ? error.response.message
+        : 'We encountered an unexpected error while fetching a resource. Please try again later or get in contact with us.';
+      throw new Error(errorMessage);
+    } else {
+      throw error
+    }
   }
-
-  return data;
 };
