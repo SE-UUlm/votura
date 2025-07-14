db4a477192b654fde898bad67aeb1074a1aedb10
JKortmann <j.e.kortmann@gmail.com>
2025-07-14 09:58:02 +0200

    #127: refactor credentials handling

--- apps/frontend/src/swr/api.ts
+++ apps/frontend/src/swr/api.ts
@@ -2,7 +2,7 @@ import { apiTokenUserObject } from '@repo/votura-validators';
 import axios, { type AxiosRequestConfig } from 'axios';
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
 import { apiRoutes } from './apiRoutes.ts';
-import {clearAuthLocalStorage, getAuthLocalStorage} from './authTokens.ts';
+import { clearAuthLocalStorage, getAuthLocalStorage } from './authTokens.ts';
 
 interface FailedRequest {
   response: {
@@ -15,23 +15,27 @@ export const api = axios.create({
 });
 
 createAuthRefreshInterceptor(api, async (failedRequest: FailedRequest) => {
-  const authToken = getAuthLocalStorage()
+  const authToken = getAuthLocalStorage();
 
   if (!authToken) {
-    clearAuthLocalStorage()
-    throw new Error('No refresh token found in local storage')
+    clearAuthLocalStorage();
+    return;
   }
 
-  const response = await api.post(apiRoutes.users.refreshTokens, {
-    refreshToken: authToken.refreshToken
-  }, {
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Authorization: `Bearer ${authToken.accessToken}`,
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      'Content-Type': 'application/json'
-    }
-  });
+  const response = await api.post(
+    apiRoutes.users.refreshTokens,
+    {
+      refreshToken: authToken.refreshToken,
+    },
+    {
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: `Bearer ${authToken.accessToken}`,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+      },
+    },
+  );
 
   const parsed = await apiTokenUserObject.safeParseAsync(response.data);
 
