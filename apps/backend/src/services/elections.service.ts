decf3ff99f1c740a94055a71c98fba056079d735
Thorben Carl <thorben.carl@gmail.com>
2025-07-14 11:45:44 +0200

    #201: fix typo

--- apps/backend/src/services/elections.service.ts
+++ apps/backend/src/services/elections.service.ts
@@ -120,7 +120,7 @@ export const unfreezeElection = async (electionId: Election['id']): Promise<Sele
     .executeTakeFirstOrThrow();
 
   // TODO: Add here the functionality to delete all voter tokens that have access to this election. (see #214)
-  // Thing about using a transaction here to ensure consistency.
+  // Think about using a transaction here to ensure consistency.
   // https://kysely.dev/docs/category/transactions
 
   return electionTransformer(election);
