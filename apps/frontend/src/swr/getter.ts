50e61989619bebf65a2b28628a6a77459829ae5e
JKortmann <j.e.kortmann@gmail.com>
2025-07-14 09:58:42 +0200

    #127: refactor to use credentials if existent

--- apps/frontend/src/swr/getter.ts
+++ apps/frontend/src/swr/getter.ts
@@ -1,25 +1,31 @@
 import axios from 'axios';
 import { api } from './api.ts';
+import { getAuthLocalStorage } from './authTokens.ts';
 import { hasMessage } from './hasMessage.ts';
 
 export const getter = async (url: string): Promise<unknown> => {
   try {
+    const authTokens = getAuthLocalStorage();
+
     const response = await api.get(url, {
       headers: {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         Accept: 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        ...(authTokens ? { Authorization: `Bearer ${authTokens.accessToken}` } : {}),
       },
     });
 
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      const errorMessage = hasMessage(error.response)
-        ? error.response.message
-        : 'We encountered an unexpected error while fetching a resource. Please try again later or get in contact with us.';
+      const errorMessage =
+        error.response?.data !== undefined && hasMessage(error.response.data)
+          ? error.response.data.message
+          : 'We encountered an unexpected error while fetching a resource. Please try again later or get in contact with us.';
       throw new Error(errorMessage);
     } else {
-      throw error
+      throw error;
     }
   }
 };
--- apps/frontend/src/swr/putter.ts
+++ apps/frontend/src/swr/putter.ts
@@ -1,24 +1,30 @@
 import axios from 'axios';
 import { api } from './api.ts';
+import { getAuthLocalStorage } from './authTokens.ts';
 import { hasMessage } from './hasMessage.ts';
 
 export const putter = async <T>(url: string, args: { arg: T }): Promise<unknown> => {
   try {
+    const authTokens = getAuthLocalStorage();
+
     const response = await api.put(url, args.arg, {
       headers: {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         Accept: 'application/json',
         // eslint-disable-next-line @typescript-eslint/naming-convention
         'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        ...(authTokens ? { Authorization: `Bearer ${authTokens.accessToken}` } : {}),
       },
     });
 
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      const errorMessage = hasMessage(error.response)
-        ? error.response.message
-        : 'We encountered an unexpected error while updating a resource. Please try again later or get in contact with us.';
+      const errorMessage =
+        error.response?.data !== undefined && hasMessage(error.response.data)
+          ? error.response.data.message
+          : 'We encountered an unexpected error while updating a resource. Please try again later or get in contact with us.';
       throw new Error(errorMessage);
     } else {
       throw error;
