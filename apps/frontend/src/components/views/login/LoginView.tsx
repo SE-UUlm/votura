be557cf012ee3439fcf862afcf7f8b68027634da
JKortmann <j.e.kortmann@gmail.com>
2025-07-14 09:59:41 +0200

    #127: add login page

--- apps/frontend/src/components/views/login/LoginView.tsx
+++ apps/frontend/src/components/views/login/LoginView.tsx
@@ -1,3 +1,102 @@
+import {
+  Anchor,
+  Button,
+  Center,
+  Container,
+  Divider,
+  Group,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useToggle } from '@mantine/hooks';
+import { notifications } from '@mantine/notifications';
+import { insertableUserObject } from '@repo/votura-validators';
+import { useNavigate } from 'react-router';
+import { setAuthLocalStorage } from '../../../swr/authTokens.ts';
+import { useLoginUser } from '../../../swr/useLoginUser.ts';
+
 export const LoginView = () => {
-  return 'hello world';
+  const { trigger, isMutating } = useLoginUser();
+  const navigate = useNavigate();
+
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: {
+      email: (value) => {
+        const parsed = insertableUserObject.shape.email.safeParse(value);
+        return parsed.success ? null : parsed.error.issues.map((issue) => issue.message).join('. ');
+      },
+    },
+  });
+
+  const [isLoginIn, toggleIsLoginIn] = useToggle();
+
+  const onLogin = async () => {
+    toggleIsLoginIn();
+    const validationResult = form.validate();
+
+    if (validationResult.hasErrors) {
+      toggleIsLoginIn();
+      return;
+    }
+
+    const data = form.getValues();
+    try {
+      const response = await trigger(data);
+      setAuthLocalStorage(response);
+      navigate('/elections');
+    } catch (e) {
+      notifications.show({
+        title: 'Could not login',
+        message: 'We do not know this combination of email and password. Please try again.',
+        color: 'yellow',
+        autoClose: 15000,
+      });
+    }
+
+    toggleIsLoginIn();
+  };
+
+  return (
+    <Container fluid h={'100vh'}>
+      <Center h={'100vh'}>
+        <Stack w={400}>
+          <Title>Votura</Title>
+          <TextInput
+            withAsterisk
+            label={'Email'}
+            placeholder={'user@votura.org'}
+            key={form.key('email')}
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            withAsterisk
+            label={'Password'}
+            placeholder={'My secure password...'}
+            key={form.key('password')}
+            {...form.getInputProps('password')}
+          />
+          <Button onClick={onLogin} loading={isLoginIn || isMutating}>
+            Login
+          </Button>
+          <Button variant="light">Sign Up</Button>
+          <Divider />
+          <Group justify="space-between">
+            <Text size={'sm'}>Can't login anymore?</Text>
+            <Anchor component={'button'} variant="transparent" size={'sm'}>
+              Reset password
+            </Anchor>
+          </Group>
+        </Stack>
+      </Center>
+    </Container>
+  );
 };
