0dd31b2e35d118af77c2a1ee99ff48cf48ee067d
David Sievers <david.sievers@uni-ulm.de>
2025-07-21 10:03:58 +0200

    #211: formatting
    Signed-off-by: David Sievers <david.sievers@uni-ulm.de>

--- apps/backend/test/voterGroups/createVoterGroup.test.ts
+++ apps/backend/test/voterGroups/createVoterGroup.test.ts
@@ -9,9 +9,9 @@ import request from 'supertest';
 import { beforeAll, describe, expect, it } from 'vitest';
 import { app } from '../../src/app.js';
 import { generateUserTokens } from '../../src/auth/utils.js';
+import { VoterGroupValidationErrorMessage } from '../../src/controllers/bodyChecks/voterGroupChecks.js';
 import { HttpStatusCode } from '../../src/httpStatusCode.js';
 import { createUser, findUserBy } from '../../src/services/users.service.js';
-import { VoterGroupValidationErrorMessage } from '../../src/controllers/bodyChecks/voterGroupChecks.js';
 import {
   demoBallotPaper,
   demoBallotPaper2,
