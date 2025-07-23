46853e8c3554389cc9d66f3291cfb540819890aa
Sören S. Hoffmann <125372709+shoffma@users.noreply.github.com>
2025-07-23 11:01:01 +0200

    test DateTimePicker with date only

--- packages/e2e/tests/mutateElectionModal.test.ts
+++ packages/e2e/tests/mutateElectionModal.test.ts
@@ -13,12 +13,6 @@ async function openModal(page: Page): Promise<void> {
   await expect(page.getByText('Create new election')).toBeVisible();
 }
 
-async function pickDateTime(page: Page, gridcell: string): Promise<void> {
-  await page.getByRole('button', { name: gridcell }).click();
-  await page.getByRole('spinbutton', { name: 'hour' }).fill('12');
-  await page.getByRole('spinbutton', { name: 'minute' }).fill('00');
-}
-
 test.describe('MutateElectionModal logic', () => {
   test.beforeEach(async ({ page }) => {
     await login(page);
@@ -43,7 +37,7 @@ test.describe('MutateElectionModal logic', () => {
 
   test('EndDateTime picker becomes enabled after setting StartDateTime', async ({ page }) => {
     await page.getByLabel('Start of voting period').click();
-    await pickDateTime(page, '10');
+    await page.getByRole('button', { name: '10' }).click();
     await expect(page.getByLabel('End of voting period')).toBeEnabled();
   });
 
@@ -51,14 +45,14 @@ test.describe('MutateElectionModal logic', () => {
     page,
   }) => {
     await page.getByLabel('Start of voting period').click();
-    await pickDateTime(page, '10');
+    await page.getByRole('button', { name: '10' }).click();
     await page.getByLabel('End of voting period').click();
-    await pickDateTime(page, '11');
+    await page.getByRole('button', { name: '11' }).click();
     await expect(page.getByLabel('End of voting period')).not.toHaveValue('');
 
     await page.getByLabel('Start of voting period').click();
     await page.getByRole('button', { name: 'Next month' }).click();
-    await pickDateTime(page, '10');
+    await page.getByRole('button', { name: '10' }).click();
     await expect(page.getByLabel('End of voting period')).toHaveValue('');
   });
 });
