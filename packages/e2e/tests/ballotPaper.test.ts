d773edd1ae02fa4e012cffd848ed5f76fa3601f3
JKortmann <j.e.kortmann@gmail.com>
2025-07-24 11:16:27 +0200

    #246: refactor waiting for pipeline

--- packages/e2e/tests/ballotPaper.test.ts
+++ packages/e2e/tests/ballotPaper.test.ts
@@ -21,7 +21,8 @@ test('should create a ballot paper', async ({ page }) => {
     .nth(1)
     .click();
   await page.getByRole('button', { name: 'New Ballot Paper' }).click();
-  await page.getByRole('textbox', { name: 'Name' }).fill(ballotPaper.name);
+  const nameTextbox = page.getByRole('textbox', { name: 'Name' })
+  await nameTextbox.fill(ballotPaper.name);
   if (ballotPaper.description !== undefined) {
     await page.getByRole('textbox', { name: 'Description' }).fill(ballotPaper.description);
   }
@@ -32,9 +33,9 @@ test('should create a ballot paper', async ({ page }) => {
     .getByRole('textbox', { name: 'Maximum votes per candidate' })
     .fill(ballotPaper.maxVotesPerCandidate.toString());
   await page.getByRole('button', { name: 'Create new ballot paper' }).click();
-  await expect(page.getByText(ballotPaper.name, { exact: true })).toBeHidden({ timeout: 10000 }); // wait for the drawer to close
-  await expect(page.getByText(ballotPaper.name, { exact: true })).toBeVisible();
+  await expect(nameTextbox).not.toBeAttached();
+  await expect(page.getByText(ballotPaper.name, { exact: true }).first()).toBeVisible();
   if (ballotPaper.description !== undefined) {
-    await expect(page.getByText(ballotPaper.description, { exact: true })).toBeVisible();
+    await expect(page.getByText(ballotPaper.description, { exact: true }).first()).toBeVisible();
   }
 });
