f9ef53fcc8dd1f098d512eb4466a8e287ea36747
Sören S. Hoffmann <125372709+shoffma@users.noreply.github.com>
2025-07-28 07:58:47 +0200

    replace modal-test with drawer-test

--- packages/e2e/tests/mutateElectionDrawer.test.ts
+++ packages/e2e/tests/mutateElectionDrawer.test.ts
@@ -8,18 +8,18 @@ async function login(page: Page): Promise<void> {
   await expect(page).toHaveURL('/elections');
 }
 
-async function openModal(page: Page): Promise<void> {
+async function openDrawer(page: Page): Promise<void> {
   await page.getByRole('button', { name: 'New Election' }).click();
   await expect(page.getByText('Create new election')).toBeVisible();
 }
 
-test.describe('MutateElectionModal logic', () => {
+test.describe('MutateElectionDrawer logic', () => {
   test.beforeEach(async ({ page }) => {
     await login(page);
-    await openModal(page);
+    await openDrawer(page);
   });
 
-  test('MutateElectionModal shows all required fields', async ({ page }) => {
+  test('MutateElectionDrawer shows all required fields', async ({ page }) => {
     await expect(page.getByLabel('Name')).toBeVisible();
     await expect(page.getByLabel('Description')).toBeVisible();
     await expect(page.getByLabel('Start of voting period')).toBeVisible();
@@ -45,17 +45,15 @@ test.describe('MutateElectionModal logic', () => {
     page,
   }) => {
     const startPicker = page.getByLabel('Start of voting period');
-    const startPickerID = await startPicker.getAttribute('aria-controls');
     const endPicker = page.getByLabel('End of voting period');
-    const endPickerID = await endPicker.getAttribute('aria-controls');
 
     await startPicker.click();
-    await page.locator(`#${startPickerID}`).getByRole('button', { name: '10' }).click();
+    await page.getByRole('button', { name: '10' }).nth(0).click();
     await endPicker.click();
-    await page.locator(`#${endPickerID}`).getByRole('button', { name: '11' }).click();
+    await page.getByRole('button', { name: '11' }).nth(1).click();
     await expect(endPicker).not.toHaveText('');
 
-    await page.locator(`#${startPickerID}`).getByRole('button', { name: '12' }).click();
+    await page.getByRole('button', { name: '12' }).nth(0).click();
     await expect(endPicker).toHaveText('');
   });
 });
