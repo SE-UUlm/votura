48e542a692b5f6b9b86bc1652b8ae5573bb8e307
JKortmann <j.e.kortmann@gmail.com>
2025-07-30 15:43:30 +0200

    #185: add return type

--- apps/frontend/src/utils/notifications.ts
+++ apps/frontend/src/utils/notifications.ts
@@ -42,14 +42,18 @@ export const getAddSuccessBallotPaperConfig = (
   };
 };
 
-export const getMutateSuccessBallotPaperConfig = (name: SelectableBallotPaper['name']) => {
+export const getMutateSuccessBallotPaperConfig = (
+  name: SelectableBallotPaper['name'],
+): NotificationData => {
   return {
     title: 'Success',
     message: `The changes to the ballot paper "${name}" have been saved.`,
   };
 };
 
-export const getDeleteSuccessBallotPaperConfig = (name: SelectableBallotPaper['name']) => {
+export const getDeleteSuccessBallotPaperConfig = (
+  name: SelectableBallotPaper['name'],
+): NotificationData => {
   return {
     title: 'Success',
     message: `The ballot paper "${name}" has been deleted.`,
