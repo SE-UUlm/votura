4ec18c814aced2e1c22d44ba3ba83e93f37a8306
Thorben Carl <thorben.carl@gmail.com>
2025-07-30 15:27:56 +0200

    #208: remove dead code

--- packages/votura-validators/src/parserParams.ts
+++ packages/votura-validators/src/parserParams.ts
@@ -4,11 +4,4 @@ import { voturaMetadataRegistry } from './voturaMetadateRegistry.js';
 export const toJsonSchemaParams: Parameters<typeof z.toJSONSchema>[1] = {
   metadata: voturaMetadataRegistry,
   unrepresentable: 'any',
-  override: (ctx) => {
-    const def = ctx.zodSchema._zod.def;
-
-    if (def.type === 'bigint') {
-      ctx.jsonSchema.type = 'string';
-    }
-  },
 };
