2ce96f978ef41a908f80e1d8a2e48ae0941e1c18
dsiev <133495111+dsiev@users.noreply.github.com>
2025-09-18 13:40:28 +0000

    #329: Add tests for ballot paper section decryption class
    More tests are necessary to test the actual decryption. This can be done after #330 is done and a ballot paper can be encrypted.
    
    Signed-off-by: GitHub <noreply@github.com>

---
+++ packages/votura-ballot-box/test/decryption/ballotPaperSection.test.ts
@@ -0,0 +1,115 @@
+import { filledBallotPaperObject } from '@repo/votura-validators';
+import { getKeyPair, type Ciphertext, type KeyPair } from '@votura/votura-crypto/index';
+import { beforeAll, describe, expect, it } from 'vitest';
+import { BallotPaperSectionDecryption } from '../../src/decryption/ballotPaperSection.js';
+
+describe('BallotPaperSectionDecryption tests', () => {
+  enum UUIDs {
+    ballotPaper = '2d38c96f-42ac-4c3f-9734-7193e9c64424',
+    section1 = 'f87cff56-13a4-4c69-9021-3aa864100ca8',
+    section2 = 'b434e057-fb55-4e38-800c-4f23d9df27f7',
+    candidate1 = '9112c5e8-53ef-44cb-b9aa-47a97b0cae51',
+    candidate2 = 'f792d5ca-65aa-4167-8f83-06e909051005',
+    candidate3 = '1c0f870f-4c85-4cf0-9a00-078f3f93737c',
+  }
+  const dummyVote = {
+    alpha: BigInt(5),
+    beta: BigInt(3),
+    commitment1: BigInt(7),
+    commitment2: BigInt(11),
+    challenge: BigInt(13),
+    response: BigInt(17),
+  };
+
+  const dummySection = {
+    votes: [
+      {
+        [UUIDs.candidate1]: { ...dummyVote },
+        [UUIDs.candidate2]: { ...dummyVote },
+        noVote: { ...dummyVote },
+        invalid: { ...dummyVote },
+      },
+      {
+        [UUIDs.candidate1]: { ...dummyVote },
+        [UUIDs.candidate2]: { ...dummyVote },
+        noVote: { ...dummyVote },
+        invalid: { ...dummyVote },
+      },
+    ],
+  };
+  let decryption: BallotPaperSectionDecryption | null = null;
+  let keyPair: KeyPair | null = null;
+
+  beforeAll(async () => {
+    keyPair = await getKeyPair(20);
+    decryption = new BallotPaperSectionDecryption(keyPair.privateKey);
+  });
+
+  it('should throw if lookup table not initialized', () => {
+    const section = {
+      votes: [],
+    };
+
+    expect(() => decryption?.decryptSection(section, 'section-1')).toThrowError(
+      'Lookup table not initialized. Call calculateLookupTable() first.',
+    );
+  });
+
+  it('should throw if calculateLookupTable called with negative maxVotes', () => {
+    expect(() => {
+      decryption?.calculateLookupTable(-1);
+    }).toThrowError('maxVotes must be a non-negative integer');
+  });
+
+  it('should calculate lookup table without errors', () => {
+    expect(() => {
+      decryption?.calculateLookupTable(10);
+    }).not.toThrow();
+  });
+
+  it('should extract candidate IDs correctly', () => {
+    const parseResult = filledBallotPaperObject.safeParse({
+      ballotPaperId: UUIDs.ballotPaper,
+      sections: {
+        [UUIDs.section1]: dummySection,
+      },
+    });
+    expect(parseResult.success).toBe(true);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    const candidateIds = (decryption as any).extractCandidateIds(dummySection);
+    expect(candidateIds).toEqual([UUIDs.candidate1, UUIDs.candidate2, 'invalid', 'noVote']); // manually ordered in alphabetical order
+  });
+
+  it('should extract all ciphertexts correctly', () => {
+    const parseResult = filledBallotPaperObject.safeParse({
+      ballotPaperId: UUIDs.ballotPaper,
+      sections: {
+        [UUIDs.section1]: dummySection,
+      },
+    });
+    expect(parseResult.success).toBe(true);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    const ciphertexts: Ciphertext[][] = (decryption as any).extractAllCiphertexts(
+      dummySection,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      (decryption as any).extractCandidateIds(dummySection),
+    );
+    console.log('Extracted ciphertexts:', ciphertexts);
+    expect(ciphertexts).toEqual([
+      [
+        [dummyVote.alpha, dummyVote.beta], // candidate 1
+        [dummyVote.alpha, dummyVote.beta], // candidate 2
+        [dummyVote.alpha, dummyVote.beta], // invalid
+        [dummyVote.alpha, dummyVote.beta], // noVote
+      ],
+      [
+        [dummyVote.alpha, dummyVote.beta], // candidate 1
+        [dummyVote.alpha, dummyVote.beta], // candidate 2
+        [dummyVote.alpha, dummyVote.beta], // invalid
+        [dummyVote.alpha, dummyVote.beta], // noVote
+      ],
+    ]);
+  });
+});
