aa0466fe8cc3b5cd838bac17ef517b81e5cb6aa0
dsiev <133495111+dsiev@users.noreply.github.com>
2025-09-19 09:43:04 +0000

    #308: Formatting
    Signed-off-by: GitHub <noreply@github.com>

--- apps/frontend/src/swr/elections/useFreezeElection.ts
+++ apps/frontend/src/swr/elections/useFreezeElection.ts
@@ -1,22 +1,19 @@
-import {
-    type SelectableElection,
-    selectableElectionObject,
-} from '@repo/votura-validators';
+import { type SelectableElection, selectableElectionObject } from '@repo/votura-validators';
 import { mutate } from 'swr';
 import useSWRMutation, { type SWRMutationResponse } from 'swr/mutation';
 import { apiRoutes } from '../apiRoutes.ts';
 import { putterFactory } from '../putterFactory.ts';
 
 export const useFreezeElection = (
-    electionId: SelectableElection['id'],
+  electionId: SelectableElection['id'],
 ): SWRMutationResponse<SelectableElection, Error, string, undefined> => {
-    return useSWRMutation(
-        apiRoutes.elections.freeze(electionId),
-        putterFactory(selectableElectionObject),
-        {
-            onSuccess: () => {
-                void mutate(apiRoutes.elections.base);
-            },
-        },
-    );
+  return useSWRMutation(
+    apiRoutes.elections.freeze(electionId),
+    putterFactory(selectableElectionObject),
+    {
+      onSuccess: () => {
+        void mutate(apiRoutes.elections.base);
+      },
+    },
+  );
 };
