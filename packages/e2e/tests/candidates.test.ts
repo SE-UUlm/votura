d7c1f5406698f819c1684174dd735cc0d08576e3
Sören S. Hoffmann <125372709+shoffma@users.noreply.github.com>
2025-10-21 22:40:15 +0200

    #187: button not found => .first()

--- packages/e2e/tests/candidates.test.ts
+++ packages/e2e/tests/candidates.test.ts
@@ -11,14 +11,14 @@ test.describe('Candidates', () => {
 
   test('should create/link, unlink and delete a candidate', async ({ page }) => {
     await page.getByRole('button', { name: 'Settings' }).nth(1).click();
-    await page.getByRole('button', { name: 'Section Settings' }).click();
+    await page.getByRole('button', { name: 'Section Settings' }).first().click();
     await page.getByRole('menuitem', { name: 'Add candidate' }).click();
     await page.getByRole('textbox', { name: 'Name' }).fill('John Doe');
     await page.getByRole('textbox', { name: 'Description' }).fill('John Doe Description');
     await page.getByRole('button', { name: 'Create Candidate' }).click();
     await expect(page.getByText('Candidates: 1')).toBeVisible();
 
-    await page.getByRole('button', { name: 'Section Settings' }).click();
+    await page.getByRole('button', { name: 'Section Settings' }).first().click();
     await page.getByRole('menuitem', { name: 'Edit candidates' }).click();
     await expect(page.getByRole('heading', { name: 'All Candidates' })).toBeVisible();
     await expect(page.getByText('John Doe', { exact: true }).first()).toBeVisible();
