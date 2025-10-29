fa9f5c06ec41a4721b86ecaa8e1215da8d3be94f
Sören S. Hoffmann <125372709+shoffma@users.noreply.github.com>
2025-10-29 14:36:44 +0100

    #187: secure existing BPS before candidates test

--- packages/e2e/tests/candidates.test.ts
+++ packages/e2e/tests/candidates.test.ts
@@ -1,4 +1,12 @@
 import { expect, test } from '@playwright/test';
+import type { UpdateableBallotPaperSection } from '@repo/votura-validators';
+
+const bpSection: UpdateableBallotPaperSection = {
+  name: 'My BPS',
+  description: 'My BPS Description',
+  maxVotes: 5,
+  maxVotesPerCandidate: 2,
+};
 
 test.describe('Candidates', () => {
   test.beforeEach(async ({ page }) => {
@@ -15,13 +23,32 @@ test.describe('Candidates', () => {
       .getByLabel('Details')
       .first()
       .click();
-    await page.getByRole('button', { name: 'Ballot Paper Settings' }).click();
-    await expect(page.getByText('Create a section to get started.')).toHaveCount(0);
-    const sectionsButtons = page.getByRole('button', { name: 'Section Settings' });
-    if ((await sectionsButtons.count()) === 0) {
-      throw new Error('No sections found - cannot open Section Settings');
+
+    if ((await page.getByText('Create a section to get started.').count()) > 0) {
+      await page.getByRole('menuitem', { name: 'Add ballot paper section' }).click();
+      await page.getByRole('textbox', { name: 'Name' }).fill(bpSection.name);
+      if (bpSection.description !== undefined) {
+        await page.getByRole('textbox', { name: 'Description' }).fill(bpSection.description);
+      }
+      await page
+        .getByRole('textbox', {
+          name: 'Maximum votes',
+          exact: true,
+        })
+        .fill(bpSection.maxVotes.toString());
+      await page
+        .getByRole('textbox', { name: 'Maximum votes per candidate' })
+        .fill(bpSection.maxVotesPerCandidate.toString());
+      const createButton = page.getByRole('button', { name: 'Create Section' });
+      await createButton.click();
+      await expect(createButton).not.toBeVisible();
+      await expect(page.getByText(bpSection.name, { exact: true }).first()).toBeVisible();
+      if (bpSection.description !== undefined) {
+        await expect(page.getByText(bpSection.description).first()).toBeVisible();
+      }
     }
-    await sectionsButtons.last().click();
+
+    await page.getByRole('button', { name: 'Section Settings' }).last().click();
     await page.getByRole('menuitem', { name: 'Add candidate' }).click();
     await page.getByRole('textbox', { name: 'Name' }).fill('John Doe');
     await page.getByRole('textbox', { name: 'Description' }).fill('John Doe Description');
