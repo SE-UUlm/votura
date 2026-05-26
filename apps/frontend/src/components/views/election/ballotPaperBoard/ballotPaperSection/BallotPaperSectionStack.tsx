1c2d3be72a5b9a77cc3b247bae3c0e9fa4bb90ff
JensOstertag <dev@jens-ostertag.de>
2026-05-26 20:29:21 +0200

    Run prettier

--- apps/frontend/src/components/views/election/ballotPaperBoard/ballotPaperSection/BallotPaperSectionStack.tsx
+++ apps/frontend/src/components/views/election/ballotPaperBoard/ballotPaperSection/BallotPaperSectionStack.tsx
@@ -30,20 +30,14 @@ export const BallotPaperSectionStack = ({
   // The ballot paper sections need to be ordered
   // Otherwise, when they are edited, it could happen that the drawer overlay remains visible
   const stackItems = data
-      ?.sort((a, b) => (a.createdAt >= b.createdAt ? 1 : -1))
-      .map((section) => (
-          <BallotPaperSection
-              key={section.id}
-              ballotPaperSection={section}
-              electionId={electionId}
-          />
-      ));
+    ?.sort((a, b) => (a.createdAt >= b.createdAt ? 1 : -1))
+    .map((section) => (
+      <BallotPaperSection key={section.id} ballotPaperSection={section} electionId={electionId} />
+    ));
 
   return (
     <ScrollArea flex={1} w={'100%'} style={{ overflowY: 'auto' }}>
-      <Stack>
-        {stackItems}
-      </Stack>
+      <Stack>{stackItems}</Stack>
     </ScrollArea>
   );
 };
