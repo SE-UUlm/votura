9bfae7986a690893c7de99f86d39018759b9a604
JensOstertag <dev@jens-ostertag.de>
2026-07-03 23:11:19 +0200

    124: Maybe it finds the alert by role?

--- packages/e2e/tests/login.test.ts
+++ packages/e2e/tests/login.test.ts
@@ -20,7 +20,7 @@ test('should reject invalid email format', async ({ page }) => {
   await page.getByLabel('Email').fill('foo');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect(page.locator('.mantine-Notifications-notification')).toBeVisible();
+  await expect(page.getByRole('alert')).toBeVisible();
   await expect(page).toHaveURL('/login');
 });
 
