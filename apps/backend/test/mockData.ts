beefbeb28f58748f35241e661292620ebe94077a
JensOstertag <dev@jens-ostertag.de>
2026-07-27 16:41:23 +0200

    456: Fix mock data

--- apps/backend/test/mockData.ts
+++ apps/backend/test/mockData.ts
@@ -14,10 +14,12 @@ const STRONG_PWD = 'MyStrong!Password123';
 export const demoUser = insertableUserObject.parse({
   email: 'user@votura.org',
   password: STRONG_PWD,
+  role: 1,
 });
 export const demoUser2 = insertableUserObject.parse({
   email: 'user2@votura.org',
   password: STRONG_PWD,
+  role: 0,
 });
 
 /*
