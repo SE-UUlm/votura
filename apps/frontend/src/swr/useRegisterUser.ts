212f9f11219d07e47cd2575cf9234c826d700d7a
JensOstertag <dev@jens-ostertag.de>
2026-07-27 16:33:45 +0200

    456: Run prettier again

--- apps/frontend/src/swr/useRegisterUser.ts
+++ apps/frontend/src/swr/useRegisterUser.ts
@@ -3,6 +3,11 @@ import useSWRMutation, { type SWRMutationResponse } from 'swr/mutation';
 import { apiRoutes } from './apiRoutes.ts';
 import { posterFactory } from './posterFactory.ts';
 
-export const useRegisterUser = (): SWRMutationResponse<void, Error, string, AuthenticatableUser> => {
+export const useRegisterUser = (): SWRMutationResponse<
+  void,
+  Error,
+  string,
+  AuthenticatableUser
+> => {
   return useSWRMutation(apiRoutes.users.base, posterFactory());
 };
