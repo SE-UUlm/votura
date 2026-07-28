39ac96e5a92bc78c9d82603e226f01adbc20ee42
JensOstertag <dev@jens-ostertag.de>
2026-07-28 14:38:50 +0200

    456: Pass role in createUser.test.ts

--- apps/backend/test/users/createUser.test.ts
+++ apps/backend/test/users/createUser.test.ts
@@ -18,6 +18,7 @@ describe(`POST /users`, () => {
     const res = await request(app).post(requestPath).send({
       email: 'invalid-email',
       password: 'short',
+      role: 1,
     });
     expect(res.status).toBe(HttpStatusCode.badRequest);
     expect(res.type).toBe('application/json');
