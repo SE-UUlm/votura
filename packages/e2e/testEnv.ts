a20d46420ab17ed70e12d5a3345a5d72804441da
JensOstertag <dev@jens-ostertag.de>
2026-07-28 16:41:11 +0200

    432: Fix void expression linting problem

--- packages/e2e/testEnv.ts
+++ packages/e2e/testEnv.ts
@@ -95,7 +95,7 @@ export const stopTestEnv = async (): Promise<void> => {
       await new Promise<void>((resolve) => {
         const killer = spawn('taskkill', ['/pid', String(backendProcess?.pid), '/T', '/F']);
 
-        killer.on('close', () => resolve());
+        killer.on('close', () => { resolve(); });
       });
     } else {
       backendProcess.kill();
