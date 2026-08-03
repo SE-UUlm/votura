c1760c9c1bf83f449fd4fb37a6bc50df17efc890
JensOstertag <dev@jens-ostertag.de>
2026-08-03 16:56:19 +0200

    497: Add email as alt description on avatar

--- apps/frontend/src/components/Avatar.tsx
+++ apps/frontend/src/components/Avatar.tsx
@@ -85,5 +85,5 @@ export const Avatar = ({ userId, email }: AvatarProps): JSX.Element => {
   const privateAvatarSvg = renderToString(<PrivateAvatar userId={userId} email={email} />);
   const avatarSrc = 'data:image/svg+xml,' + encodeURIComponent(privateAvatarSvg);
 
-  return <MantineAvatar src={avatarSrc} />;
+  return <MantineAvatar src={avatarSrc} alt={email} />;
 };
