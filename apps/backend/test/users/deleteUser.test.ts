c9b489b36048b6e813986b6ed151ce634ee5ae2f
JensOstertag <dev@jens-ostertag.de>
2026-08-11 17:35:08 +0200

    507: Adjust delete user test

--- apps/backend/test/users/deleteUser.test.ts
+++ apps/backend/test/users/deleteUser.test.ts
@@ -5,12 +5,9 @@ import { app } from '../../src/app.js';
 import { generateUserTokens } from '../../src/auth/utils.js';
 import { HttpStatusCode } from '../../src/httpStatusCode.js';
 import { createUser, findUserBy } from '../../src/services/users.service.js';
-import { demoUser } from '../mockData.js';
+import {demoUser, demoUser2} from '../mockData.js';
 
-// TODO: Modify BAPI endpoints
-
-describe(`DELETE /users`, () => {
-  const requestPath = '/users';
+describe(`DELETE /users/{userId}`, () => {
   let tokens: ApiTokenUser = { accessToken: '', refreshToken: '' };
 
   beforeAll(async () => {
@@ -24,6 +21,13 @@ describe(`DELETE /users`, () => {
   });
 
   it('204: should delete a user', async () => {
+    const user2 = await findUserBy({ email: demoUser2.email });
+    if (user2 === null) {
+      throw new Error('Failed to find test user 2');
+    }
+
+    const requestPath = `/users/${user2.id}`;
+
     const res = await request(app)
       .delete(requestPath)
       .set('Authorization', `Bearer ${tokens.accessToken}`);
@@ -31,5 +35,16 @@ describe(`DELETE /users`, () => {
     expect(res.status).toBe(HttpStatusCode.noContent);
     const result = await findUserBy({ email: demoUser.email });
     expect(result).toBeNull();
+  });
+
+  it('404: should return 404 if user not found', async () => {
+    const nonexistentUserId = '00000000-0000-0000-0000-000000000000';
+    const requestPath = `/users/${nonexistentUserId}`;
+
+    const res = await request(app)
+      .delete(requestPath)
+      .set('Authorization', `Bearer ${tokens.accessToken}`);
+
+    expect(res.status).toBe(HttpStatusCode.notFound);
   });
 });
