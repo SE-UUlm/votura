c4e1f7cbda1544d39110c41e10aa54d94eee23ab
JensOstertag <dev@jens-ostertag.de>
2026-08-11 18:08:50 +0200

    531: Fix linting problems

--- apps/backend/src/controllers/users/getUsers.uc.ts
+++ apps/backend/src/controllers/users/getUsers.uc.ts
@@ -1,5 +1,3 @@
-import type { Request, Response } from 'express';
-
-export const getUsers = async (req: Request, res: Response): Promise<void> => {
+export const getUsers = async (): Promise<void> => {
   // TODO #503: https://github.com/SE-UUlm/votura/issues/503
 };
