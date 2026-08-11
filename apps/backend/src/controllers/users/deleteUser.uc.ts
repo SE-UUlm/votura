ac3076ca4cca1fdde1ebd3644d641d9b0230ad4f
JensOstertag <dev@jens-ostertag.de>
2026-08-11 17:00:41 +0200

    507: Edit delete user endpoint to require a user id

--- apps/backend/src/controllers/users/deleteUser.uc.ts
+++ apps/backend/src/controllers/users/deleteUser.uc.ts
@@ -1,12 +1,27 @@
-import type { SelectableUser } from '@repo/votura-validators';
+import {
+  type Response404,
+  response404Object,
+  type SelectableUser,
+  type User,
+} from '@repo/votura-validators';
 import type { Request, Response } from 'express';
 import { HttpStatusCode } from '../../httpStatusCode.js';
-import { deleteUser as deletePersistentUser } from '../../services/users.service.js';
+import { deleteUser as deletePersistentUser, findUserBy } from '../../services/users.service.js';
 
 export const deleteUser = async (
-  _req: Request,
-  res: Response<void, { user: SelectableUser }>,
+  req: Request<{ userId: User['id'] }>,
+  res: Response<void | Response404, { user: SelectableUser }>,
 ): Promise<void> => {
-  await deletePersistentUser(res.locals.user.id);
+  const userToDelete = await findUserBy({ id: req.params.userId });
+  if (userToDelete === null) {
+    res.status(HttpStatusCode.notFound).json(
+      response404Object.parse({
+        message: 'User not found.',
+      }),
+    );
+    return;
+  }
+
+  await deletePersistentUser(userToDelete.id);
   res.sendStatus(HttpStatusCode.noContent);
 };
