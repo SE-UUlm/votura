0861aafff5adc5c0f143bddd60f06bca2992cb0b
JensOstertag <dev@jens-ostertag.de>
2026-08-13 11:09:02 +0200

    503: getUsers API endpoint

--- apps/backend/src/controllers/users/getUsers.uc.ts
+++ apps/backend/src/controllers/users/getUsers.uc.ts
@@ -1,3 +1,8 @@
-export const getUsers = async (): Promise<void> => {
-  // TODO #503: https://github.com/SE-UUlm/votura/issues/503
+import type { SelectableUser } from '@repo/votura-validators';
+import type { Request, Response } from 'express';
+import { getAllUsers } from '../../services/users.service.js';
+
+export const getUsers = async (_req: Request, res: Response<SelectableUser[]>): Promise<void> => {
+  const users = await getAllUsers();
+  res.status(200).json(users);
 };
