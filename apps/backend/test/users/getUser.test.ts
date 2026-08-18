8ebd3c7119ddd372c395bc89373ade3436dfc8bb
JensOstertag <dev@jens-ostertag.de>
2026-08-18 22:01:51 +0200

    504: More tests for get user endpoint

--- apps/backend/test/users/getUser.test.ts
+++ apps/backend/test/users/getUser.test.ts
@@ -10,27 +10,33 @@ import { generateUserTokens } from '../../src/auth/utils.js';
 import { HttpStatusCode } from '../../src/httpStatusCode.js';
 import { UserAuthErrorMessages } from '../../src/middlewares/auth.js';
 import { createUser, findUserBy } from '../../src/services/users.service.js';
-import { demoUser } from '../mockData.js';
+import { demoUser, demoUser2 } from '../mockData.js';
 
 describe(`GET /users/{userId}`, () => {
-  let requestPath = '';
-  let tokens: ApiTokenUser = { accessToken: '', refreshToken: '' };
+  let requestPath1 = '';
+  let requestPath2 = '';
+  let tokens1: ApiTokenUser = { accessToken: '', refreshToken: '' };
+  let tokens2: ApiTokenUser = { accessToken: '', refreshToken: '' };
 
   beforeAll(async () => {
     await createUser(demoUser);
-    const user = await findUserBy({ email: demoUser.email });
-    if (user === null) {
-      throw new Error('Failed to find test user');
+    await createUser(demoUser2);
+    const user1 = await findUserBy({ email: demoUser.email });
+    const user2 = await findUserBy({ email: demoUser2.email });
+    if (user1 === null || user2 === null) {
+      throw new Error('Failed to find test users');
     }
 
-    tokens = generateUserTokens(user.id);
-    requestPath = `/users/${user.id}`;
+    tokens1 = generateUserTokens(user1.id);
+    tokens2 = generateUserTokens(user2.id);
+    requestPath1 = `/users/${user1.id}`;
+    requestPath2 = `/users/${user2.id}`;
   });
 
   it('200: should return the account details of the requesting user', async () => {
     const res = await request(app)
-      .get(requestPath)
-      .set('Authorization', `Bearer ${tokens.accessToken}`);
+      .get(requestPath2)
+      .set('Authorization', `Bearer ${tokens2.accessToken}`);
 
     expect(res.status).toBe(HttpStatusCode.ok);
     expect(res.type).toBe('application/json');
@@ -41,12 +47,43 @@ describe(`GET /users/{userId}`, () => {
     }
   });
 
+  it('200: should return the account details of another user if admin', async () => {
+    const res = await request(app)
+      .get(requestPath2)
+      .set('Authorization', `Bearer ${tokens1.accessToken}`);
+
+    expect(res.status).toBe(HttpStatusCode.ok);
+    expect(res.type).toBe('application/json');
+    const parseResult = selectableUserObject.safeParse(res.body);
+    expect(parseResult.success).toBe(true);
+    if (parseResult.success) {
+      expect(parseResult.data.email).toBe(demoUser2.email);
+    }
+  });
+
   it('401: should return error for missing access token', async () => {
-    const res = await request(app).get(requestPath);
+    const res = await request(app).get(requestPath1);
     expect(res.status).toBe(HttpStatusCode.unauthorized);
     expect(res.type).toBe('application/json');
     const parseResult = response401Object.safeParse(res.body);
     expect(parseResult.success).toBe(true);
     expect(parseResult.data?.message).toBe(UserAuthErrorMessages.noToken);
+  });
+
+  it('403: should not allow to get account details of another user if not admin', async () => {
+    const res = await request(app)
+      .get(requestPath1)
+      .set('Authorization', `Bearer ${tokens2.accessToken}`);
+
+    expect(res.status).toBe(HttpStatusCode.forbidden);
+  });
+
+  it('404: should return error for non-existing user', async () => {
+    const nonexistentRequestPath = '/users/01234567-89ab-cdef-0123-456789abcdef';
+    const res = await request(app)
+      .get(nonexistentRequestPath)
+      .set('Authorization', `Bearer ${tokens1.accessToken}`);
+
+    expect(res.status).toBe(HttpStatusCode.notFound);
   });
 });
