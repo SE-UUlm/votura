175320ec0365d4f4d90d94643f53fd704aea8c5b
JensOstertag <dev@jens-ostertag.de>
2026-08-25 22:01:16 +0200

    502: Mutate user list after editing

--- apps/frontend/src/swr/useEditUser.ts
+++ apps/frontend/src/swr/useEditUser.ts
@@ -2,9 +2,16 @@ import type { EditUserData, SelectableUser } from '@repo/votura-validators';
 import useSWRMutation, { type SWRMutationResponse } from 'swr/mutation';
 import { apiRoutes } from './apiRoutes.ts';
 import { posterFactory } from './posterFactory.ts';
+import {mutate} from "swr";
 
 export const useEditUser = (
   userId: SelectableUser['id'],
 ): SWRMutationResponse<void, Error, string, EditUserData> => {
-  return useSWRMutation(apiRoutes.users.editById(userId), posterFactory());
+  return useSWRMutation(apiRoutes.users.editById(userId), posterFactory(), {
+    onSuccess: () => {
+      mutate(apiRoutes.users.all).catch((error) => {
+        console.error('Failed to mutate users', error);
+      });
+    }
+  });
 };
